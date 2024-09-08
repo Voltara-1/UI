6c49183328ec5567d38ae4a767f37f7b76977474
Voltara-1 <wmp122788@gmail.com>
2024-09-07 23:16:58 -0400

    Update HL.lua

--- HL.lua
+++ HL.lua
@@ -1,20 +1,55 @@
--- not my script ty for leting me use it
-
 local highlighter = {}
+
 local keywords = {
 	lua = {
 		"and", "break", "or", "else", "elseif", "if", "then", "until", "repeat", "while", "do", "for", "in", "end",
-		"local", "return", "function", "export"
+		"local", "return", "function", "export", "not", "nil", "true", "false", "goto", "self", "assert", "error",
+		"pcall", "xpcall", "require", "module", "setmetatable", "getmetatable", "load", "loadfile", "dofile"
 	},
 	rbx = {
-		"game", "workspace", "script", "math", "string", "table", "task", "wait", "select", "next", "Enum",
-		"error", "warn", "tick", "assert", "shared", "loadstring", "tonumber", "tostring", "type",
-		"typeof", "unpack", "print", "Instance", "CFrame", "Vector3", "Vector2", "Color3", "UDim", "UDim2", "Ray", "BrickColor",
-		"OverlapParams", "RaycastParams", "Axes", "Random", "Region3", "Rect", "TweenInfo",
-		"collectgarbage", "not", "utf8", "pcall", "xpcall", "_G", "setmetatable", "getmetatable", "os", "pairs", "ipairs"
+		"game", "workspace", "script", "math", "string", "table", "task", "wait", "select", "next", "Enum", "Instance",
+		"error", "warn", "tick", "assert", "shared", "loadstring", "tonumber", "tostring", "type", "typeof", "unpack",
+		"print", "Instance", "CFrame", "Vector3", "Vector2", "Color3", "UDim", "UDim2", "Ray", "BrickColor",
+		"OverlapParams", "RaycastParams", "Axes", "Random", "Region3", "Rect", "TweenInfo", "Vector3int16",
+		"collectgarbage", "utf8", "pcall", "xpcall", "_G", "setmetatable", "getmetatable", "os", "pairs", "ipairs",
+		"tick", "GetChildren", "FindFirstChild", "GetService", "Clone", "Destroy", "FindFirstChildOfClass", "WaitForChild",
+		"GetPlayerFromCharacter", "UserInputService", "Players", "Lighting", "TweenService", "RunService", "Debris",
+		"Players", "Teams", "Chat", "NetworkClient", "NetworkServer", "ReplicatedStorage", "StarterGui", "StarterPack",
+		"StarterCharacterScripts", "PlayerScripts", "CoreGui", "CorePackages", "CoreScripts", "ContentProvider"
+	},
+	synapse = {
+		"getgenv", "getrenv", "getsenv", "getfenv", "setfenv", "getreg", "getgc", "getupvalues", "setupvalue", "getupvalue",
+		"setreadonly", "isreadonly", "hookfunction", "debug.getinfo", "debug.setupvalue", "debug.getupvalue", "newcclosure",
+		"checkcaller", "fireclickdetector", "firetouchinterest", "isfile", "isfolder", "makefolder", "writefile", "delfile",
+		"delfolder", "readfile", "appendfile", "loadstring", "HttpGet", "HttpGetAsync", "request", "getconnections", "fireproximityprompt",
+		"getnamecallmethod", "setnamecallmethod", "hookmetamethod", "getrawmetatable", "setrawmetatable", "identifyexecutor", 
+		"is_synapse_function", "syn_crypt_b64_encode", "syn_crypt_b64_decode", "syn_crypt_encrypt", "syn_crypt_decrypt",
+		"gethui", "isrbxactive", "syn.secure_call", "syn_context_set", "syn_context_get", "syn.request",
+		"syn_getgc", "syn_setmetatable", "syn_getmetatable", "syn_random", "syn_is_prototype", "syn_create_prototype"
+	},
+	functions = {
+		"print", "warn", "error", "assert", "type", "typeof", "tonumber", "tostring", "loadstring", "select", "spawn",
+		"delay", "wait", "pcall", "xpcall", "pairs", "ipairs", "coroutine.create", "coroutine.resume", "coroutine.yield",
+		"coroutine.status", "coroutine.wrap", "debug.traceback", "debug.getupvalue", "debug.setupvalue", "debug.getinfo",
+		"string.char", "string.byte", "string.sub", "string.find", "string.match", "string.gsub", "string.format", "table.insert",
+		"table.remove", "table.sort", "table.concat", "table.pack", "table.unpack", "math.random", "math.randomseed",
+		"math.max", "math.min", "math.abs", "math.sin", "math.cos", "math.tan", "math.sqrt", "math.floor", "math.ceil"
+	},
+	properties = {
+		"Name", "ClassName", "Parent", "Position", "Size", "Color", "Transparency", "AnchorPoint", "Visible",
+		"BackgroundColor3", "BorderSizePixel", "Text", "TextColor3", "TextSize", "TextStrokeTransparency", "BackgroundTransparency",
+		"TextTransparency", "Image", "ImageRectOffset", "ImageRectSize", "ZIndex", "Rotation", "Material", "BrickColor", "Reflectance",
+		"CanCollide", "CanQuery", "CanTouch", "Anchored", "Locked", "Massless", "Velocity", "AssemblyLinearVelocity", "AssemblyAngularVelocity",
+		"AssemblyMass", "SizeConstraint", "UICorner", "UIStroke", "UITextSizeConstraint", "UITextButton", "UIGridLayout", "UIListLayout",
+		"UIAspectRatioConstraint", "UIScale", "TextBounds", "TextLabel", "TextButton", "TextBox", "ScrollingFrame", "Frame", "ImageLabel",
+		"ImageButton", "TextButton", "TextLabel", "BillboardGui", "SurfaceGui", "ViewportFrame"
 	},
 	operators = {
-		"#", "+", "-", "*", "%", "/", "^", "=", "~", "=", "<", ">", ",", ".", "(", ")", "{", "}", "[", "]", ";", ":"
+		"#", "+", "-", "*", "%", "/", "^", "=", "~=", "<", ">", ",", ".", "(", ")", "{", "}", "[", "]", ";", ":", "::",
+		"==", "<=", ">=", "..", "::", "==", "not", "or", "and", "break", "continue", "return", "goto", "self", "elseif", "end"
+	},
+	comment_delimiters = {
+		"--", "--[[", "--]]"
 	}
 }
 
@@ -33,6 +68,8 @@ local colors = {
 	function_color = Color3.fromRGB(241, 122, 124),
 	self_color = Color3.fromRGB(146, 134, 234),
 	local_property = Color3.fromRGB(129, 222, 255),
+	keyword_color = Color3.fromRGB(188, 169, 100), 
+	property_color = Color3.fromRGB(177, 144, 224), 
 }
 
 local function createKeywordSet(keywords)
@@ -45,7 +82,10 @@ end
 
 local luaSet = createKeywordSet(keywords.lua)
 local rbxSet = createKeywordSet(keywords.rbx)
+local synapseSet = createKeywordSet(keywords.synapse)
 local operatorsSet = createKeywordSet(keywords.operators)
+local functionsSet = createKeywordSet(keywords.functions)
+local propertiesSet = createKeywordSet(keywords.properties)
 
 local function getHighlight(tokens, index)
 	local token = tokens[index]
@@ -62,9 +102,11 @@ local function getHighlight(tokens, index)
 		return colors.comment
 	elseif operatorsSet[token] then
 		return colors.operator
-	elseif luaSet[token] then
+	elseif luaSet[token] or functionsSet[token] then
+		return colors.lua
+	elseif rbxSet[token] or propertiesSet[token] then
 		return colors.rbx
-	elseif rbxSet[token] then
+	elseif synapseSet[token] then
 		return colors.lua
 	elseif token:sub(1, 1) == "\"" or token:sub(1, 1) == "\'" then
 		return colors.str
@@ -80,92 +122,23 @@ local function getHighlight(tokens, index)
 		return colors.call
 	end
 
-	if tokens[index - 1] == "." then
-		if tokens[index - 2] == "Enum" then
-			return colors.rbx
-		end
-
-		return colors.local_property
+	if propertiesSet[token] then
+		return colors.property_color
 	end
+
+	return colors.null
 end
 
-function highlighter.run(source)
+local function highlightText(text)
 	local tokens = {}
-	local currentToken = ""
-	
-	local inString = false
-	local inComment = false
-	local commentPersist = false
-	
-	for i = 1, #source do
-		local character = source:sub(i, i)
-		
-		if inComment then
-			if character == "\n" and not commentPersist then
-				table.insert(tokens, currentToken)
-				table.insert(tokens, character)
-				currentToken = ""
-				
-				inComment = false
-			elseif source:sub(i - 1, i) == "]]" and commentPersist then
-				currentToken ..= "]"
-				
-				table.insert(tokens, currentToken)
-				currentToken = ""
-				
-				inComment = false
-				commentPersist = false
-			else
-				currentToken = currentToken .. character
-			end
-		elseif inString then
-			if character == inString and source:sub(i-1, i-1) ~= "\\" or character == "\n" then
-				currentToken = currentToken .. character
-				inString = false
-			else
-				currentToken = currentToken .. character
-			end
-		else
-			if source:sub(i, i + 1) == "--" then
-				table.insert(tokens, currentToken)
-				currentToken = "-"
-				inComment = true
-				commentPersist = source:sub(i + 2, i + 3) == "[["
-			elseif character == "\"" or character == "\'" then
-				table.insert(tokens, currentToken)
-				currentToken = character
-				inString = character
-			elseif operatorsSet[character] then
-				table.insert(tokens, currentToken)
-				table.insert(tokens, character)
-				currentToken = ""
-			elseif character:match("[%w_]") then
-				currentToken = currentToken .. character
-			else
-				table.insert(tokens, currentToken)
-				table.insert(tokens, character)
-				currentToken = ""
-			end
-		end
-	end
-	
-	table.insert(tokens, currentToken)
-
 	local highlighted = {}
-	
-	for i, token in ipairs(tokens) do
-		local highlight = getHighlight(tokens, i)
 
-		if highlight then
-			local syntax = string.format("<font color = \"#%s\">%s</font>", highlight:ToHex(), token:gsub("<", "&lt;"):gsub(">", "&gt;"))
-			
-			table.insert(highlighted, syntax)
-		else
-			table.insert(highlighted, token)
-		end
+	for i, token in ipairs(tokens) do
+		local color = getHighlight(tokens, i)
+		table.insert(highlighted, {text = token, color = color})
 	end
 
-	return table.concat(highlighted)
+	return highlighted
 end
 
 return highlighter
